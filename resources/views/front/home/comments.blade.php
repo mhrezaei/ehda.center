c6e5fc20d58715d69f3290de5cfc684fb5306ead
EmiTis Yousefi <emitis89@yahoo.com>
2017-04-23 10:34:33 +0430

    define new settings for comments
    1. add option for dashboard comment and use it
    2. show "custommers-comment" text in dashboard
    3. define "title2" meta for commenting posts and use it in home page

--- resources/views/front/home/comments.blade.php
+++ resources/views/front/home/comments.blade.php
@@ -1,20 +1,21 @@
-@if($comments->count())
-<section id="testimonials">
-    <div class="simple-title"> مشتریان در موردمان چه می‌گویند؟</div>
-    <div class="testimonials-list">
-        @foreach($comments as $comment)
-            {{ null , $comment->spreadMeta() }}
-            <div class="item">
-                <div class="content">
-                    <div class="text">
-                        <p>
-                            {{ $comment->text }}
-                        </p>
+@if($comments->count() and setting()->ask('dashboard_comment')->gain())
+    {{ null, $commentingPost->spreadMeta() }}
+    <section id="testimonials">
+        <div class="simple-title">{{ $commentingPost->title2 }}</div>
+        <div class="testimonials-list">
+            @foreach($comments as $comment)
+                {{ null , $comment->spreadMeta() }}
+                <div class="item">
+                    <div class="content">
+                        <div class="text">
+                            <p>
+                                {{ $comment->text }}
+                            </p>
+                        </div>
+                        <div class="person">{{ $comment->creator->full_name }}</div>
                     </div>
-                    <div class="person">{{ $comment->creator->full_name }}</div>
                 </div>
-            </div>
-        @endforeach
-    </div>
-</section>
+            @endforeach
+        </div>
+    </section>
 @endif
--- resources/views/front/user/dashboard/add_comment.blade.php
+++ resources/views/front/user/dashboard/add_comment.blade.php
@@ -1,3 +1,4 @@
+{{ null, $post->spreadMeta() }}
 <div class="container">
     <div class="row">
         <div class="col-sm-8 col-center">
@@ -6,6 +7,9 @@
                     <div class="title"><span class="icon-comment"></span> {{ $post->title }} </div>
                 </header>
                 <article>
+                    <div class="col-xs-12 pb15">
+                        {{ $post->text }}
+                    </div>
                     @include('front.frame.widgets.comment_form')
                 </article>
             </section>
