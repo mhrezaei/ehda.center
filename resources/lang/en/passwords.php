e9a7bfb9f4bb8a77f3a0d9de0d9af9f69e5f6ae1
EmiTis Yousefi <emitis89@yahoo.com>
2017-05-15 17:10:56 +0430

    Sync english and persian lang files

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -12,15 +12,15 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password'        => 'Password Not Safe',
-    'reset'           => 'Your password has been reset.',
-    'sent'            => 'Token has been sent.',
-    'token'           => 'Invalid Token',
-    'token_expired'   => 'Token has been expired.',
-    'token_verified'  => 'Token has been verified',
-    'get_new_token'   => 'Get New Token',
-    'user'            => "No User Fount",
-    'reset_sms'       => 'Your Reset Token: ::token',
-    'reset_mail'      => "کد بازیابی رمز عبور شما: ::token سلام. \n\r",
-    'reset_mail'      => "Hello,\n\rYour reset token: ::token",
+    'password'       => 'Password Not Safe',
+    'reset'          => 'Your password has been reset.',
+    'sent'           => 'Token has been sent.',
+    'token'          => 'Invalid Token',
+    'token_expired'  => 'Token has been expired.',
+    'token_verified' => 'Token has been verified',
+    'get_new_token'  => 'Get New Token',
+    'user'           => "No User Fount",
+    'reset_sms'      => 'Your Reset Token: ::token',
+    'reset_mail'     => "کد بازیابی رمز عبور شما: ::token سلام. \n\r",
+    'reset_mail'     => "Hello,\n\rYour reset token: ::token",
 ];
