1c289c6184939c8398aaf4503cdbdbbb7bcc8b34
EmiTis Yousefi <emitis89@yahoo.com>
2017-06-14 17:45:21 +0430

    Gallery Single and Archive Page

---
+++ resources/views/front/gallery/single/item.blade.php
@@ -0,0 +1,5 @@
+<div class="flex-item">
+    <a href="https://ehda.center/assets/photos/posts/slideshow/s02.jpg" class="inner">
+        <img src="http://lorempicsum.com/nemo/350/350/{{ ($i % 9) + 1 }}" alt="عنوان تصویر">
+    </a>
+</div>
