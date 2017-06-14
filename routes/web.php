fdff1b663a2b546e550d0abf42559977bab6c1cf
EmiTis Yousefi <emitis89@yahoo.com>
2017-06-14 13:11:15 +0430

    Routes Synced

--- routes/web.php
+++ routes/web.php
@@ -31,167 +31,164 @@ Route::get('manage/heyCheck', 'Front\FrontController@heyCheck');
 |
 */
 Route::group([
-	'prefix' => "manage",
-	'middleware' => ['auth', 'is:admin'],
-	'namespace' => "Manage",
+    'prefix'     => "manage",
+    'middleware' => ['auth', 'is:admin'],
+    'namespace'  => "Manage",
 ], function () {
-	Route::get('/', 'HomeController@index');
-	Route::get('/index', 'HomeController@index');
-	Route::get('/account', 'HomeController@account');
-	Route::post('/password', 'HomeController@changePassword');
+    Route::get('/', 'HomeController@index');
+    Route::get('/index', 'HomeController@index');
+    Route::get('/account', 'HomeController@account');
+    Route::post('/password', 'HomeController@changePassword');
 
-	/*-----------------------------------------------
-	| Users ...
-	*/
-	Route::group(['prefix' => "users", 'middleware' => "can:users",], function () {
-		Route::get('/update/{item_id}/{request_role}', 'UsersController@update');
-		Route::get('browse/{role}/search/{keyword?}', 'UsersController@search');
-		Route::get('browse/{role}/{request_tab?}', 'UsersController@browse');
-		Route::get('create/{role}', 'UsersController@create');
-		Route::get('/act/{model_id}/{action}/{option?}', 'UsersController@singleAction');
+    /*-----------------------------------------------
+    | Users ...
+    */
+    Route::group(['prefix' => "users", 'middleware' => "can:users",], function () {
+        Route::get('/update/{item_id}/{request_role}', 'UsersController@update');
+        Route::get('browse/{role}/search/{keyword?}', 'UsersController@search');
+        Route::get('browse/{role}/{request_tab?}', 'UsersController@browse');
+        Route::get('create/{role}', 'UsersController@create');
+        Route::get('/act/{model_id}/{action}/{option?}', 'UsersController@singleAction');
 
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('/', 'UsersController@save');
-			Route::post('/password', 'UsersController@savePassword');
-			Route::post('/permits', 'UsersController@savePermits');
-			Route::get('/role/{user_id}/{role_slug}/{new_status}' , 'UsersController@saveRole');
-			Route::post('/delete', 'UsersController@delete');
-			Route::post('/undelete', 'UsersController@undelete');
-			Route::post('/destroy', 'UsersController@destroy');
-			Route::post('/receipt' , 'UsersController@saveNewReceipt');
-		});
-	});
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('/', 'UsersController@save');
+            Route::post('/password', 'UsersController@savePassword');
+            Route::post('/permits', 'UsersController@savePermits');
+            Route::get('/role/{user_id}/{role_slug}/{new_status}', 'UsersController@saveRole');
+            Route::post('/delete', 'UsersController@delete');
+            Route::post('/undelete', 'UsersController@undelete');
+            Route::post('/destroy', 'UsersController@destroy');
+            Route::post('/receipt', 'UsersController@saveNewReceipt');
+        });
+    });
 
-	/*-----------------------------------------------
-	| Comments ...
-	*/
-	Route::group(['prefix' => 'comments' , 'middleware' => 'can:comments'] , function() {
-		Route::get('/update/{item_id}', 'CommentsController@update');
-		Route::get('/act/{model_id}/{action}', 'CommentsController@singleAction');
-		Route::get('{request_tab?}/{switches?}' , 'CommentsController@browse');
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('/', 'CommentsController@save');
-			Route::post('/process' , 'CommentsController@process');
-			Route::post('/delete', 'CommentsController@delete');
-			Route::post('/deleteMass' , 'CommentsController@deleteMass');
-			Route::post('/undelete', 'CommentsController@undelete');
-			Route::post('/undeleteMass', 'CommentsController@undeleteMass');
-			Route::post('/destroy', 'CommentsController@destroy');
-			Route::post('/destroyMass', 'CommentsController@destroyMass');
-			Route::post('/statusMass', 'CommentsController@statusMass');
-		});
+    /*-----------------------------------------------
+    | Comments ...
+    */
+    Route::group(['prefix' => 'comments', 'middleware' => 'can:comments'], function () {
+        Route::get('/update/{item_id}', 'CommentsController@update');
+        Route::get('/act/{model_id}/{action}', 'CommentsController@singleAction');
+        Route::get('{request_tab?}/{switches?}', 'CommentsController@browse');
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('/', 'CommentsController@save');
+            Route::post('/process', 'CommentsController@process');
+            Route::post('/delete', 'CommentsController@delete');
+            Route::post('/deleteMass', 'CommentsController@deleteMass');
+            Route::post('/undelete', 'CommentsController@undelete');
+            Route::post('/undeleteMass', 'CommentsController@undeleteMass');
+            Route::post('/destroy', 'CommentsController@destroy');
+            Route::post('/destroyMass', 'CommentsController@destroyMass');
+            Route::post('/statusMass', 'CommentsController@statusMass');
+        });
 
-	});
-
-
-
-	/*-----------------------------------------------
-	| Posts ...
-	*/
-	Route::group(['prefix' => 'posts'], function () {
-		Route::get('/update/{item_id}', 'PostsController@update');
-		Route::get('/tab_update/{posttype}/{request_tab?}/{switches?}', 'PostsController@tabUpdate');
-
-		Route::get('/act/{model_id}/{action}/{option?}', 'PostsController@singleAction');
-		Route::get('/check_slug/{id}/{type}/{locale}/{slug?}/p', 'PostsController@checkSlug');
-
-		Route::get('/{posttype}', 'PostsController@browse');
-		Route::get('/{posttype}/create/{locale?}/{sisterhood?}', 'PostsController@create');
-		Route::get('{posttype}/edit/{post_id}', 'PostsController@editor');
-		//		Route::get('{posttype}/searched' , 'PostsController@searchResult');
-		Route::get('{posttype}/{locale}/search', 'PostsController@search');
-		Route::get('/{posttype}/{request_tab?}/{switches?}', 'PostsController@browse');
-
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('/', 'PostsController@save');
-			Route::post('/delete', 'PostsController@delete');
-			Route::post('/undelete', 'PostsController@undelete');
-			Route::post('/destroy', 'PostsController@destroy');
-			Route::post('/clone', 'PostsController@makeClone');
-			Route::post('/deleteMass' , 'PostsController@deleteMass');
-			Route::post('/undeleteMass', 'PostsController@undeleteMass');
-			Route::post('/destroyMass', 'PostsController@destroyMass');
-			Route::post('/owner', 'PostsController@changeOwner');
-			Route::post('/good' , 'PostsController@saveGood');
-		});
-
-	});
-
-	/*-----------------------------------------------
-	| Club ...
-	*/
-	Route::group(['prefix' => "club",] , function() {
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('/draw_prepare' , 'ClubController@drawPrepare');
-			Route::post('/draw_select' , 'ClubController@drawSelect');
-			Route::get('/draw_delete/{key}' , 'ClubController@drawDelete');
-		});
-	});
+    });
 
 
+    /*-----------------------------------------------
+    | Posts ...
+    */
+    Route::group(['prefix' => 'posts'], function () {
+        Route::get('/update/{item_id}', 'PostsController@update');
+        Route::get('/tab_update/{posttype}/{request_tab?}/{switches?}', 'PostsController@tabUpdate');
+
+        Route::get('/act/{model_id}/{action}/{option?}', 'PostsController@singleAction');
+        Route::get('/check_slug/{id}/{type}/{locale}/{slug?}/p', 'PostsController@checkSlug');
+
+        Route::get('/{posttype}', 'PostsController@browse');
+        Route::get('/{posttype}/create/{locale?}/{sisterhood?}', 'PostsController@create');
+        Route::get('{posttype}/edit/{post_id}', 'PostsController@editor');
+        //		Route::get('{posttype}/searched' , 'PostsController@searchResult');
+        Route::get('{posttype}/{locale}/search', 'PostsController@search');
+        Route::get('/{posttype}/{request_tab?}/{switches?}', 'PostsController@browse');
+
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('/', 'PostsController@save');
+            Route::post('/delete', 'PostsController@delete');
+            Route::post('/undelete', 'PostsController@undelete');
+            Route::post('/destroy', 'PostsController@destroy');
+            Route::post('/clone', 'PostsController@makeClone');
+            Route::post('/deleteMass', 'PostsController@deleteMass');
+            Route::post('/undeleteMass', 'PostsController@undeleteMass');
+            Route::post('/destroyMass', 'PostsController@destroyMass');
+            Route::post('/owner', 'PostsController@changeOwner');
+            Route::post('/good', 'PostsController@saveGood');
+        });
+
+    });
+
+    /*-----------------------------------------------
+    | Club ...
+    */
+    Route::group(['prefix' => "club",], function () {
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('/draw_prepare', 'ClubController@drawPrepare');
+            Route::post('/draw_select', 'ClubController@drawSelect');
+            Route::get('/draw_delete/{key}', 'ClubController@drawDelete');
+        });
+    });
 
 
-	/*-----------------------------------------------
-	| Settings ...
-	*/
-	Route::group(['prefix' => 'settings', 'middleware' => 'can:super'], function () {
-		Route::get('/', 'SettingsController@index');
-		Route::get('/tab/{request_tab?}', 'SettingsController@index');
-		Route::get('/search', 'SettingsController@search');
-		Route::get('/update/{model_id}', 'SettingsController@update');
-		Route::get('/act/{model_id}/{action}/{option?}', 'SettingsController@singleAction');
+    /*-----------------------------------------------
+    | Settings ...
+    */
+    Route::group(['prefix' => 'settings', 'middleware' => 'can:super'], function () {
+        Route::get('/', 'SettingsController@index');
+        Route::get('/tab/{request_tab?}', 'SettingsController@index');
+        Route::get('/search', 'SettingsController@search');
+        Route::get('/update/{model_id}', 'SettingsController@update');
+        Route::get('/act/{model_id}/{action}/{option?}', 'SettingsController@singleAction');
 
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('/', 'SettingsController@save');
-			Route::post('/posttype', 'SettingsController@savePosttypeDownstream');
-			Route::post('/pack', 'SettingsController@savePack');
-		});
-	});
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('/', 'SettingsController@save');
+            Route::post('/posttype', 'SettingsController@savePosttypeDownstream');
+            Route::post('/pack', 'SettingsController@savePack');
+        });
+    });
 
-	/*-----------------------------------------------
-	| Categories ...
-	*/
-	Route::group(['prefix' => 'categories', 'middleware' => 'can:super'], function () {
-		Route::get('/' , 'CategoriesController@index');
-		Route::get('/update/{id}' , 'CategoriesController@update');
-		Route::get('/browse/{type}/{locale}' , 'CategoriesController@index');
-		Route::get('/create/folder/{type}/{locale}' , 'CategoriesController@createFolder');
-		Route::get('/create/{folder_id}/' , 'CategoriesController@createCategory');
-		Route::get('/edit/folder/{folder_id}' , 'CategoriesController@editFolder');
-		Route::get('/edit/{category_id}' , 'CategoriesController@editCategory');
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('' , 'CategoriesController@saveCategory');
-			Route::post('folder' , 'CategoriesController@saveFolder');
-		});
-	});
+    /*-----------------------------------------------
+    | Categories ...
+    */
+    Route::group(['prefix' => 'categories', 'middleware' => 'can:super'], function () {
+        Route::get('/', 'CategoriesController@index');
+        Route::get('/update/{id}', 'CategoriesController@update');
+        Route::get('/browse/{type}/{locale}', 'CategoriesController@index');
+        Route::get('/create/folder/{type}/{locale}', 'CategoriesController@createFolder');
+        Route::get('/create/{folder_id}/', 'CategoriesController@createCategory');
+        Route::get('/edit/folder/{folder_id}', 'CategoriesController@editFolder');
+        Route::get('/edit/{category_id}', 'CategoriesController@editCategory');
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('', 'CategoriesController@saveCategory');
+            Route::post('folder', 'CategoriesController@saveFolder');
+        });
+    });
 
 
-	/*-----------------------------------------------
-	| Upstream ...
-	*/
-	Route::group(['prefix' => 'upstream', 'middleware' => 'is:developer'], function () {
-		Route::get('/{request_tab?}', 'UpstreamController@index');
-		Route::get('/{request_tab}/search/', 'UpstreamController@search');
-		Route::get('/edit/{request_tab?}/{item_id?}/{parent_id?}', 'UpstreamController@editor');
-		Route::get('/{request_tab}/{item_id}/{parent_id?}', 'UpstreamController@item');
+    /*-----------------------------------------------
+    | Upstream ...
+    */
+    Route::group(['prefix' => 'upstream', 'middleware' => 'is:developer'], function () {
+        Route::get('/{request_tab?}', 'UpstreamController@index');
+        Route::get('/{request_tab}/search/', 'UpstreamController@search');
+        Route::get('/edit/{request_tab?}/{item_id?}/{parent_id?}', 'UpstreamController@editor');
+        Route::get('/{request_tab}/{item_id}/{parent_id?}', 'UpstreamController@item');
 
-		Route::group(['prefix' => 'save'], function () {
-			Route::post('role', 'UpstreamController@saveRole');
-			Route::post('role-titles', 'UpstreamController@saveRoleTitles');
-			Route::post('role-activeness', 'UpstreamController@saveRoleActiveness');
-			Route::post('role-default', 'UpstreamController@saveRoleDefault');
-			Route::post('state', 'UpstreamController@saveProvince');
-			Route::post('domain', 'UpstreamController@saveDomain');
-			Route::post('city', 'UpstreamController@saveCity');
-			Route::post('posttype', 'UpstreamController@savePosttype');
-			Route::post('posttype-titles', 'UpstreamController@savePosttypeTitles');
-			Route::post('department', 'UpstreamController@saveDepartment');
-			Route::post('category', 'UpstreamController@saveCategory');
-			Route::post('downstream', 'UpstreamController@saveDownstream');
-			Route::post('package', 'UpstreamController@savePackage');
-			Route::post('login_as', 'UpstreamController@loginAs');
-		});
-	});
+        Route::group(['prefix' => 'save'], function () {
+            Route::post('role', 'UpstreamController@saveRole');
+            Route::post('role-titles', 'UpstreamController@saveRoleTitles');
+            Route::post('role-activeness', 'UpstreamController@saveRoleActiveness');
+            Route::post('role-default', 'UpstreamController@saveRoleDefault');
+            Route::post('state', 'UpstreamController@saveProvince');
+            Route::post('domain', 'UpstreamController@saveDomain');
+            Route::post('city', 'UpstreamController@saveCity');
+            Route::post('posttype', 'UpstreamController@savePosttype');
+            Route::post('posttype-titles', 'UpstreamController@savePosttypeTitles');
+            Route::post('department', 'UpstreamController@saveDepartment');
+            Route::post('category', 'UpstreamController@saveCategory');
+            Route::post('downstream', 'UpstreamController@saveDownstream');
+            Route::post('package', 'UpstreamController@savePackage');
+            Route::post('login_as', 'UpstreamController@loginAs');
+        });
+    });
 });
 
 
@@ -202,16 +199,15 @@ Route::group([
 |
 */
 Route::group(['namespace' => 'Auth', 'prefix' => '{lang}', 'middleware' => ['DetectLanguage', 'Setting']], function () {
-	// reset password
-	Route::get('/password/reset', 'ForgotPasswordController@showLinkRequestForm');
-	Route::post('/password/reset', 'ForgotPasswordController@sendResetLinkEmail');
-	Route::get('/password/token', 'ForgotPasswordController@getToken');
-	Route::post('/password/token', 'ForgotPasswordController@checkToken');
-	Route::get('/password/new', 'ForgotPasswordController@newPassword');
-	Route::post('/password/new', 'ForgotPasswordController@changePassword');
+    // reset password
+    Route::get('/password/reset', 'ForgotPasswordController@showLinkRequestForm');
+    Route::post('/password/reset', 'ForgotPasswordController@sendResetLinkEmail');
+    Route::get('/password/token/{haveCode?}', 'ForgotPasswordController@getToken');
+    Route::post('/password/token', 'ForgotPasswordController@checkToken');
+    Route::get('/password/new', 'ForgotPasswordController@newPassword');
+    Route::post('/password/new', 'ForgotPasswordController@changePassword');
 
 });
-
 
 // uplaod url to b used in dropzone
 Route::group(['prefix' => 'file'], function () {
@@ -243,4 +239,3 @@ Route::group(['namespace' => 'Front', 'middleware' => ['DetectLanguage', 'Settin
     Route::post('/comment', 'PostController@submit_comment')->name('comment.submit');
 
 });
-
