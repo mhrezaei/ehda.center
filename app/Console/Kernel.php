0c8a96fe771c801021bdce81ca5fc004f3176cef
chieftaha <chieftaha@gmail.com>
2017-08-13 16:36:28 +0430

    cron jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,39 +2,44 @@
 
 namespace App\Console;
 
+use App\Providers\MessagesServiceProvider;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		//
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+	 *
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		$schedule->call( function() {
+			MessagesServiceProvider::sendPendingMessages() ;
+		})->everyFiveMinutes() ;
+	}
 
-    /**
-     * Register the Closure based commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        require base_path('routes/console.php');
-    }
+	/**
+	 * Register the Closure based commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		require base_path('routes/console.php');
+	}
 }
