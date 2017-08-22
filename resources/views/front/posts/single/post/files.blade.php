e6d25b1e147de924e43a9c872a052b79610ac37e
EmiTis Yousefi <emitis89@yahoo.com>
2017-08-22 19:22:55 +0430

    education no access

--- resources/views/front/posts/single/post/files.blade.php
+++ resources/views/front/posts/single/post/files.blade.php
@@ -1,26 +1,36 @@
-@if($post->canDownloadFile())
+@php
+    $canDownload = $post->canDownloadFile();
+    $noAccessPost = \App\Providers\PostsServiceProvider::smartFindPost('education-no-access-file');
+    $noAccessText = $noAccessPost->exists ? $noAccessPost->title : '';
+@endphp
+
+@if($canDownload or $noAccessText)
     <div class="row">
         <div class="col-xs-12 mt20 well bg-lightGray text-blue">
             <h5 class="text-blue">{{ trans('front.download_links') }}</h5>
-            <p>
-                @php $downloadingFiles = $post->post_files @endphp
-                @if($downloadingFiles and is_array($downloadingFiles) and count($downloadingFiles))
-                    @foreach($downloadingFiles as $key => $downloadingFile)
-                        @php
-                            $fileObj = \App\Providers\UploadServiceProvider::smartFindFile($downloadingFile['src']);
-                        @endphp
-                        @if($fileObj->exists)
+            @if($post->canDownloadFile())
+                <p>
+                    @php $downloadingFiles = $post->post_files @endphp
+                    @if($downloadingFiles and is_array($downloadingFiles) and count($downloadingFiles))
+                        @foreach($downloadingFiles as $key => $downloadingFile)
                             @php
-                                $fileObj->spreadMeta();
-                                $linkTitle = $downloadingFile['label'] ?: $fileObj->original_name;
-                                $url = route('file.download', [$downloadingFile['src'], $downloadingFile['label']]);
+                                $fileObj = \App\Providers\UploadServiceProvider::smartFindFile($downloadingFile['src']);
                             @endphp
-                            @if($key) <br/> @endif
-                            <a target="_blank" href="{{ $url }}">{{ $linkTitle }}</a>
-                        @endif
-                    @endforeach
-                @endif
-            </p>
+                            @if($fileObj->exists)
+                                @php
+                                    $fileObj->spreadMeta();
+                                    $linkTitle = $downloadingFile['label'] ?: $fileObj->original_name;
+                                    $url = route('file.download', [$downloadingFile['src'], $downloadingFile['label']]);
+                                @endphp
+                                @if($key) <br/> @endif
+                                <a target="_blank" href="{{ $url }}">{{ $linkTitle }}</a>
+                            @endif
+                        @endforeach
+                    @endif
+                </p>
+            @else
+                <div class="alert alert-danger">{{ $noAccessText }}</div>
+            @endif
         </div>
     </div>
 @endif
