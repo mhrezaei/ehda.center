e378dd5daddbd95e6c57a8d0d8e12b4361eaa221
EmiTis Yousefi <emitis89@yahoo.com>
2017-08-27 09:50:41 +0430

    debug ecg

--- resources/views/front/ecg/frame/scripts.blade.php
+++ resources/views/front/ecg/frame/scripts.blade.php
@@ -4,6 +4,6 @@
 
 {{ Html::script('assets/libs/jquery/jquery-3.2.1.min.js') }}
 {{ Html::script('assets/libs/bootstrap/js/bootstrap.min.js') }}
-{{ Html::script('assets/libs/lowLag/lowLag.min.js') }}
-{{ Html::script('assets/libs/lowLag/soundmanager2.min.js') }}
+{{ Html::script('assets/libs/low-lag/low-lag.min.js') }}
+{{ Html::script('assets/libs/low-lag/soundmanager2.min.js') }}
 {{ Html::script('assets/js/tools.min.js') }}
