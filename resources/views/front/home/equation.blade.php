5b610a6a9be89bbda182547c0360bc429352cbb2
EmiTis Yousefi <emitis89@yahoo.com>
2017-10-24 12:58:34 +0330

    Fix direction problem of quation in home page of english website

--- resources/views/front/home/equation.blade.php
+++ resources/views/front/home/equation.blade.php
@@ -1,5 +1,5 @@
 @if($equationPost and $equationPost->exists)
     <div class="row fixed-background" style="background-image:url({{ url('assets/images/image-1.jpg') }})">
-        <h3 class="text-white text-center" style="line-height:3em;font-size:450%;height:3em">{{ ad($equationPost->title) }}</h3>
+        <h3 class="text-white text-center" style="line-height:3em;font-size:450%;height:3em; direction: rtl">{{ ad($equationPost->title) }}</h3>
     </div>
 @endif
