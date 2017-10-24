fb5fc2ed362192f1b82548e07d0ce035e6e1e522
EmiTis Yousefi <emitis89@yahoo.com>
2017-10-24 13:53:11 +0330

    Apply a setting to find out posts of which domains should be shown

--- resources/views/manage/comments/show-reply.blade.php
+++ resources/views/manage/comments/show-reply.blade.php
@@ -26,31 +26,33 @@
     'self_label' => trans('posts.comments.reply_via_email_too') ,
 ])
 
-@include('forms.group-start')
-<div class="row">
-    <div class="col-sm-3">
-        @include('forms.button' , [
-            'label' => trans('posts.form.refer_to'),
-            'shape' => 'info',
-            'type' => 'button',
-            'class' => 'btn-xs',
-            'link' => '$(".refer-target-selector").slideDown()'
-        ])
+@if($supportRoles and $supportRoles->count())
+    @include('forms.group-start')
+    <div class="row">
+        <div class="col-sm-3">
+            @include('forms.button' , [
+                'label' => trans('posts.form.refer_to'),
+                'shape' => 'info',
+                'type' => 'button',
+                'class' => 'btn-xs',
+                'link' => '$(".refer-target-selector").slideDown()'
+            ])
+        </div>
+        <div class="col-sm-5 refer-target-selector" style="display: none">
+            @include("forms.select_self", [
+                'name' => "department_id",
+                'value' => $parent->department_id ,
+                'options' => $supportRoles ,
+                'blank_value' => '',
+                'class' => 'btn-xs',
+                'extra' => 'data-style="btn-xs btn-default"',
+            ])
+        </div>
     </div>
-    <div class="col-sm-5 refer-target-selector" style="display: none">
-        @include("forms.select_self", [
-            'name' => "department_id",
-            'value' => $parent->department_id ,
-            'options' => $supportRoles ,
-            'blank_value' => '',
-            'class' => 'btn-xs',
-            'extra' => 'data-style="btn-xs btn-default"',
-        ])
-    </div>
-</div>
+    @include('forms.group-end')
+@endif
 
 
-@include('forms.group-end')
 
 @include('forms.group-start')
 
