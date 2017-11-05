bf1a56bbbf8c3f42337f825fceaf13f915e4022b
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2017-11-05 15:54:45 +0330

    add samandehi logo

--- resources/views/front/frame/useful_links.blade.php
+++ resources/views/front/frame/useful_links.blade.php
@@ -15,3 +15,4 @@
         @endforeach
     @endif
 </div>
+<img id='sizpjzpeoeukesgtsizp' style='cursor:pointer' onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=97809&p=pfvljyoemcsiobpdpfvl", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")' alt='logo-samandehi' src='https://logo.samandehi.ir/logo.aspx?id=97809&p=bsiyyndtaqgwlymabsiy'/>
